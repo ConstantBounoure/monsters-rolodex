c0717159f762e2e19a3a58c64be06aa133e4a949
ConstantBounoure <constant.bounoure@gmail.com>
2023-01-25 13:30:03 +0100

    added components and styling

---
+++ src/components/search-box/search-box-.component.jsx
@@ -0,0 +1,23 @@
+import { Component } from "react";
+import "./search-box.styles.css";
+
+class SearchBox extends Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        const { onChangeHandler, placeholder, className } = this.props;
+
+        return (
+            <input
+                type="search"
+                className={`search-box ${className}`}
+                placeholder={placeholder}
+                onChange={onChangeHandler}
+            ></input>
+        );
+    }
+}
+
+export default SearchBox;
