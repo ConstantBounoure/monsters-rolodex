6fbe54898eccbfabff11ac84a58361764e29d8e7
ConstantBounoure <constant.bounoure@gmail.com>
2023-01-26 13:40:43 +0100

    project done, learning func comp

--- src/App.js
+++ src/App.js
@@ -1,59 +1,69 @@
 import { Component } from "react";
+
 import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box-.component";
+
 import "./App.css";
 
-class App extends Component {
-    constructor() {
-        super();
+const App = () => {
+    return (
+        <div className="App">
+            <h1 className="app-title">Monsters Rolodex</h1>
+        </div>
+    );
+};
 
-        this.state = {
-            monsters: [],
-            searchField: "",
-        };
-    }
+// class App extends Component {
+//     constructor() {
+//         super();
 
-    componentDidMount() {
-        fetch("https://jsonplaceholder.typicode.com/users")
-            .then((response) => response.json())
-            .then((users) =>
-                this.setState(
-                    () => {
-                        return { monsters: users };
-                    },
-                    () => console.log(this.state)
-                )
-            );
-    }
+//         this.state = {
+//             monsters: [],
+//             searchField: "",
+//         };
+//     }
 
-    onSearchChange = (event) => {
-        const searchField = event.target.value.toLowerCase();
-        this.setState(() => {
-            return { searchField };
-        });
-    };
+//     componentDidMount() {
+//         fetch("https://jsonplaceholder.typicode.com/users")
+//             .then((response) => response.json())
+//             .then((users) =>
+//                 this.setState(
+//                     () => {
+//                         return { monsters: users };
+//                     },
+//                     () => console.log(this.state)
+//                 )
+//             );
+//     }
 
-    render() {
-        // get component assets
-        const { monsters, searchField } = this.state;
-        const { onSearchChange } = this;
+//     onSearchChange = (event) => {
+//         const searchField = event.target.value.toLowerCase();
+//         this.setState(() => {
+//             return { searchField };
+//         });
+//     };
 
-        const filteredMonsters = monsters.filter((monster) =>
-            monster.name.toLowerCase().includes(searchField)
-        );
+//     render() {
+//         // get component assets
+//         const { monsters, searchField } = this.state;
+//         const { onSearchChange } = this;
 
-        return (
-            <div className="App">
-				<h1 className="app-title">Monsters Rolodex</h1>
-                <SearchBox
-                    onChangeHandler={onSearchChange}
-                    placeholder="search monsters"
-                    className="monsters-search-box"
-                />
-                <CardList monsters={filteredMonsters} />
-            </div>
-        );
-    }
-}
+//         const filteredMonsters = monsters.filter((monster) =>
+//             monster.name.toLowerCase().includes(searchField)
+//         );
+
+//         return (
+//             <div className="App">
+// 				<h1 className="app-title">Monsters Rolodex</h1>
+//                 <SearchBox
+//                     onChangeHandler={onSearchChange}
+//                     placeholder="search monsters"
+//                     className="monsters-search-box"
+//                 />
+//                 <CardList monsters={filteredMonsters} />
+//             </div>
+//         );
+//     }
+// }
 
 export default App;
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,22 +1,23 @@
 import { Component } from "react";
+
 import "./card.styles.css";
 
 class Card extends Component {
-	constructor() {
-		super()
-	}
+    constructor() {
+        super();
+    }
 
-	render() {
-		const { title, subtitle, src, className } = this.props;
+    render() {
+        const { title, subtitle, src, className } = this.props;
 
-		return (
-			<div className={`card-container ${className}`}>
-				<img src={src} alt={title} />
-				<h2>{title}</h2>
-				<p>{subtitle}</p>
-			</div>
-		)
-	}
+        return (
+            <div className={`card-container ${className}`}>
+                <img src={src} alt={title} />
+                <h2>{title}</h2>
+                <p>{subtitle}</p>
+            </div>
+        );
+    }
 }
 
 export default Card;
